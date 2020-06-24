de54fd9cc4b829d5d71108bb63b8e26b167fabed
jaydeep <jaydeeppatidar87@gmail.com>
2020-06-24 09:38:06 +0530

    modify all files to made an calculator more smart

--- SmartCalculator.py
+++ SmartCalculator.py
@@ -10,9 +10,18 @@ while True:
     for word in text.split():
         if word.upper() in operations.keys():
             try:
-                l=extract_numbers_from_text(text)
-                r=operations[word.upper()](l[0],l[1])
-                print(r)
+                l1=extract_numbers_from_text(text)
+                l2=extract_complex_from_text(text)
+                for x in l2:
+                    if x.imag==0:
+                        pass
+                    else:
+                      r=operations[word.upper()](l2[0],l2[1])
+                      print(r)
+                      break
+                else:
+                    r=operations[word.upper()](l1[0],l1[1])
+                    print(r)
             except:
                 print('Something went wrong...!!! please retry')
             finally:
@@ -30,10 +39,33 @@ while True:
             finally:
                 break
         elif word.upper() in operation.keys():
+         if word.upper()=='SQUARE' :
             try:
-                l=extract_numbers_from_text(text)
-                r=operation[word.upper()](int(l[0]))
-                print(r)
+                l2=extract_complex_from_text(text)
+                if len(l2)>0:
+                  for x in l2:
+                      if x.imag==0:
+                          pass
+                      else:
+                        r=operation[word.upper()](l2[0])
+                        print(r)
+                        break
+                  else:
+                    for x in l2:
+                        if type(x.real)==float:
+                            r=operation[word.upper()](x.real)
+                            print(r)
+                            break
+            except:
+                print('Something went wrong...!!! please retry')
+            finally:
+                break
+         else:
+            try:
+                    l1=extract_numbers_from_text(text)
+                    r=operation[word.upper()](int(l1[0]))
+                    print(r)
+                    break
             except:
                 print('Something went wrong...!!! please retry')
             finally:
--- module/calculations.py
+++ module/calculations.py
@@ -1,4 +1,4 @@
-Responses=['welcome to smart calculator','my name is smart calculator made by JAYDEEP PATIDAR','Thanks for using smart calculator','sorry,this is beyond my ability']
+Responses=['welcome to smart calculator','my name is smart calculator','Thanks for using smart calculator','sorry,this is beyond my ability','JAYDEEP PATIDAR \nFROM INSTITUTE OF ENGINEERING AND TECHNOLOGY - DAVV,INDORE,M.P.,INDIA','CONTACT MR. JAYDEEP PATIDAR \nCONTACT DETAILS:\nPHONE: 7745939565 \nMAIL: jaydeeppatidar87@gmail.com','you can use me as calculator i can do basic calculations like addition,subtraction,multiplication,division,logical and ,or ,xor,lcm,hcf between two numbers,square,binary,hexdecimal,octal of a number\nyou have to Enter you question with separator space between every meaningful operator and oprands,number and number,words and number\nI did not expect anything from you','nothing']
 def extract_numbers_from_text(text):
     L=[]
     for t in text.split():
@@ -7,6 +7,14 @@ def extract_numbers_from_text(text):
         except ValueError:
             pass
     return L
+def extract_complex_from_text(text):
+    l=[]
+    for t in text.split():
+        try:
+            l.append(complex(t))
+        except ValueError:
+            pass
+    return l
 def lcmoftwo(a,b):
  L=a if a>b else b
  while L<=a*b:
@@ -32,18 +40,20 @@ def div2(a,b):
 def binary(a):
     b=bin(a)
     return b[2::]
+def sqr(a):
+    return (a)**2
 def hexadecimal(a):
     h=hex(a)
     return h[2::]
 def octal(a):
     b=oct(a)
     return b[2::]
-def xorof2(a,b):
-    return a ^ b
-def orof2(a,b):
-    return a | b
-def andof2(a,b):
-    return a & b
+def xoroftwo(a,b):
+    return a^b
+def oroftwo(a,b):
+    return a|b
+def andoftwo(a,b):
+    return a&b
 def end():
     print(Responses[2])
     input('press enter key to exit')
@@ -52,7 +62,17 @@ def myname():
     print(Responses[1])
 def sorry():
     print(Responses[3])
+def made():
+    print(Responses[4])
+def contact():
+    print(Responses[5])
+def use():
+    print(Responses[6])
+def want():
+    print(Responses[7])
+def purpose():
+    print("purpose to learn and taught something")
 operations={'*':mul,'-':sub,'/':div,'//':div2,'+':add,'HCF':hcfoftwo,'LCM':lcmoftwo,'PLUS':add,'ADD':add,'ADDITION':add,'SUM':add,'MINUS':sub,'SUBTRACT':sub,'DIFFERENTIATE':sub,'DIFFERENCE':sub,'PRODUCT':mul,'MULTIPLY':mul,'DIVIDE':div,'DIVISION':div}
-commands={'NAME':myname,'CLOSE':end,'END':end,'TERMINATE':end,'EXIT':end}
-operation={'BINARY':binary,'HEX':hexadecimal,'HEXADECIMAL':hexadecimal,'OCTAL':octal,'OCT':octal,'BIN':binary}
-logical={'OR':orof2,'|':orof2,'AND':andof2,'&':andof2,'^':xorof2,'XOR':xorof2}
+commands={'CONTACT':contact,'WANT':want,'CHARGE':want,'FEE':want,'LEARN':contact,'PURPOSE':purpose,'MISSION':purpose,'VISION':purpose,'USE':use,'DO':use,'WORK':use,'ABILITY':use,'DEVLOP':made,'MADE':made,'HOW':use,'WHO':made,'DEVLOPER':made,'NAME':myname,'CLOSE':end,'END':end,'TERMINATE':end,'EXIT':end}
+operation={'SQUARE':sqr,'BINARY':binary,'HEX':hexadecimal,'HEXADECIMAL':hexadecimal,'OCTAL':octal,'OCT':octal,'BIN':binary}
+logical={'OR':oroftwo,'|':oroftwo,'AND':andoftwo,'&':andoftwo,'^':xoroftwo,'XOR':xoroftwo}
