577ea72bdd9dafc926d19b88f880e98ba830c6d3
jaydeep <jaydeeppatidar87@gmail.com>
2020-08-12 22:37:02 +0530

    fixed some errors...ls!

--- SmartCalculator.py
+++ SmartCalculator.py
@@ -6,69 +6,72 @@ print(Responses[0])
 print(Responses[1])
 while True:
     print()
-    text=input('Enter a question')
-    for word in text.split():
-        if word.upper() in operations.keys():
-            try:
-                l1=extract_numbers_from_text(text)
-                l2=extract_complex_from_text(text)
-                for x in l2:
-                    if x.imag==0:
-                        pass
-                    else:
-                      r=operations[word.upper()](l2[0],l2[1])
-                      print(r)
-                      break
-                else:
-                    r=operations[word.upper()](l1[0],l1[1])
-                    print(r)
-            except:
-                print('Something went wrong...!!! please retry')
-            finally:
-                break
-        elif word.upper() in commands.keys():
-            commands[word.upper()]()
-            break
-        elif word.upper() in logical.keys():
-            try:
-                l=extract_numbers_from_text(text)
-                r=logical[word.upper()](int(l[0]),int(l[1]))
-                print(r)
-            except:
-                print('Something went wrong...!!! please retry')
-            finally:
-                break
-        elif word.upper() in operation.keys():
-         if word.upper()=='SQUARE' :
-            try:
-                l2=extract_complex_from_text(text)
-                if len(l2)>0:
-                  for x in l2:
-                      if x.imag==0:
-                          pass
-                      else:
-                        r=operation[word.upper()](l2[0])
-                        print(r)
-                        break
-                  else:
+    text=input("Enter a question or press 'q' to exit")
+    if text=='q':
+        end()
+    else:
+        for word in text.split():
+            if word.upper() in operations.keys():
+                try:
+                    l1=extract_numbers_from_text(text)
+                    l2=extract_complex_from_text(text)
                     for x in l2:
-                        if type(x.real)==float:
-                            r=operation[word.upper()](x.real)
+                        if x.imag==0:
+                            pass
+                        else:
+                          r=operations[word.upper()](l2[0],l2[1])
+                          print(r)
+                          break
+                    else:
+                        r=operations[word.upper()](l1[0],l1[1])
+                        print(r)
+                except:
+                    print('Something went wrong...!!! please retry')
+                finally:
+                    break
+            elif word.upper() in commands.keys():
+                commands[word.upper()]()
+                break
+            elif word.upper() in logical.keys():
+                try:
+                    l=extract_numbers_from_text(text)
+                    r=logical[word.upper()](int(l[0]),int(l[1]))
+                    print(r)
+                except:
+                    print('Something went wrong...!!! please retry')
+                finally:
+                    break
+            elif word.upper() in operation.keys():
+             if word.upper()=='SQUARE' :
+                try:
+                    l2=extract_complex_from_text(text)
+                    if len(l2)>0:
+                      for x in l2:
+                          if x.imag==0:
+                              pass
+                          else:
+                            r=operation[word.upper()](l2[0])
                             print(r)
                             break
-            except:
-                print('Something went wrong...!!! please retry')
-            finally:
-                break
-         else:
-            try:
-                    l1=extract_numbers_from_text(text)
-                    r=operation[word.upper()](int(l1[0]))
-                    print(r)
+                      else:
+                        for x in l2:
+                            if type(x.real)==float:
+                                r=operation[word.upper()](x.real)
+                                print(r)
+                                break
+                except:
+                    print('Something went wrong...!!! please retry')
+                finally:
                     break
-            except:
-                print('Something went wrong...!!! please retry')
-            finally:
-                break
-    else:
-        sorry()
+             else:
+                try:
+                        l1=extract_numbers_from_text(text)
+                        r=operation[word.upper()](int(l1[0]))
+                        print(r)
+                        break
+                except:
+                    print('Something went wrong...!!! please retry')
+                finally:
+                    break
+        else:
+            sorry()
--- module/calculations.py
+++ module/calculations.py
@@ -1,4 +1,4 @@
-Responses=['welcome to smart calculator','my name is smart calculator','Thanks for using smart calculator','sorry,this is beyond my ability','JAYDEEP PATIDAR \nFROM INSTITUTE OF ENGINEERING AND TECHNOLOGY - DAVV,INDORE,M.P.,INDIA','CONTACT MR. JAYDEEP PATIDAR \nCONTACT DETAILS:\nPHONE: 7745939565 \nMAIL: jaydeeppatidar87@gmail.com','you can use me as calculator i can do basic calculations like addition,subtraction,multiplication,division,logical and ,or ,xor,lcm,hcf between two numbers,square,binary,hexdecimal,octal of a number\nyou have to Enter you question with separator space between every meaningful operator and oprands,number and number,words and number\nI did not expect anything from you','nothing']
+Responses=['\n****Welcome to smart calculator****\n','My name is smart calculator','Thanks for using smart calculator','sorry,this is beyond my ability','JAYDEEP PATIDAR \nFROM INSTITUTE OF ENGINEERING AND TECHNOLOGY-DAVV,INDORE,M.P.,INDIA','CONTACT MR. JAYDEEP PATIDAR \nCONTACT DETAILS:\nPHONE: 7745939565 \nMAIL: jaydeeppatidar87@gmail.com','you can use me as calculator i can do basic calculations like addition,subtraction,multiplication,division,logical and ,or ,xor,lcm,hcf between two numbers,square,binary,hexdecimal,octal of a number\nyou have to Enter you question with separator space between every meaningful operator and oprands,number and number,words and number\nI did not expect anything from you','nothing']
 def extract_numbers_from_text(text):
     L=[]
     for t in text.split():
@@ -26,7 +26,7 @@ def hcfoftwo(a,b):
  while H>=1:
      if a%H==0 and b%H==0:
          return H
-     h-=1
+     H-=1
 def add(a,b):
     return a+b
 def sub(a,b):
@@ -72,7 +72,7 @@ def want():
     print(Responses[7])
 def purpose():
     print("purpose to learn and taught something")
-operations={'*':mul,'-':sub,'/':div,'//':div2,'+':add,'HCF':hcfoftwo,'LCM':lcmoftwo,'PLUS':add,'ADD':add,'ADDITION':add,'SUM':add,'MINUS':sub,'SUBTRACT':sub,'DIFFERENTIATE':sub,'DIFFERENCE':sub,'PRODUCT':mul,'MULTIPLY':mul,'DIVIDE':div,'DIVISION':div}
-commands={'CONTACT':contact,'WANT':want,'CHARGE':want,'FEE':want,'LEARN':contact,'PURPOSE':purpose,'MISSION':purpose,'VISION':purpose,'USE':use,'DO':use,'WORK':use,'ABILITY':use,'DEVLOP':made,'MADE':made,'HOW':use,'WHO':made,'DEVLOPER':made,'NAME':myname,'CLOSE':end,'END':end,'TERMINATE':end,'EXIT':end}
+operations={'*':mul,'-':sub,'/':div,'//':div2,'MUL':mul,'DIV':div,'SUB':sub,'+':add,'HCF':hcfoftwo,'LCM':lcmoftwo,'PLUS':add,'ADD':add,'ADDITION':add,'SUM':add,'MINUS':sub,'SUBTRACT':sub,'DIFFERENTIATE':sub,'DIFFERENCE':sub,'PRODUCT':mul,'MULTIPLY':mul,'DIVIDE':div,'DIVISION':div}
+commands={'CONTACT':contact,'WANT':want,'CHARGE':want,'FEE':want,'LEARN':contact,'PURPOSE':purpose,'MISSION':purpose,'VISION':purpose,'USE':use,'DO':use,'WORK':use,'ABILITY':use,'DEVLOP':made,'MADE':made,'HOW':use,'WHO':made,'DEVLOPER':made,'DEVLOP':made,'NAME':myname,'CLOSE':end,'END':end,'TERMINATE':end,'EXIT':end}
 operation={'SQUARE':sqr,'BINARY':binary,'HEX':hexadecimal,'HEXADECIMAL':hexadecimal,'OCTAL':octal,'OCT':octal,'BIN':binary}
 logical={'OR':oroftwo,'|':oroftwo,'AND':andoftwo,'&':andoftwo,'^':xoroftwo,'XOR':xoroftwo}
